28fa0f8426484ba0eb5a79b6c97505a0f3de0777
vpastortejero <vpastortejero@gmail.com>
2020-04-05 13:37:02 +0200

    Añado RecyclerView

--- Ejercicio_01/app/src/main/java/com/example/ejercicio_01/MainActivity.java
+++ Ejercicio_01/app/src/main/java/com/example/ejercicio_01/MainActivity.java
@@ -3,24 +3,50 @@ package com.example.ejercicio_01;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.IntentFilter;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.LinearLayout;
-import android.widget.RadioButton;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements Dialog.Response {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements Dialog.Response, MyAdapter.ItemClickListener {
 
     DialogFragment dialogFragment = new Dialog();
+    private RecyclerView recyclerView;
+    private MyAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    //private String[] myDataSet = {"Prueba1", "Prueba2", "Prueba3"};
+    private ArrayList<String> myDataSet = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /** RECYCLERVIEW PARA GESTIONAR LA LISTA PRINCIPAL */
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        //myDataSet.add("Prueba1");
+        //myDataSet.add("Prueba2");
+        //myDataSet.add("Prueba3");
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataSet);
+        mAdapter.setClickListener(this);
+        recyclerView.setAdapter(mAdapter);
 
         /** COMPRUEBA LA CONEXION DEL CARGADOR */
         ReceiverConnect receiverConnect = new ReceiverConnect();
@@ -64,9 +90,25 @@ public class MainActivity extends AppCompatActivity implements Dialog.Response {
     /** REALIZA LAS ACCIONES FIJADAS POR EL DIALOG */
     public void onResponse(String string) {
         //Toast.makeText(this, string, Toast.LENGTH_SHORT).show();
+        /*
         LinearLayout linearLayout = findViewById(R.id.linearLayaout);
         RadioButton radioButton = new RadioButton(this);
         radioButton.setText(string);
         linearLayout.addView(radioButton);
+         */
+        myDataSet.add(string);
+        mAdapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Toast.makeText(this, "You clicked \" + mAdapter.getItem(position) + \" on row number \" + position", Toast.LENGTH_SHORT).show();
+        //Log.d("prueba", "You clicked " + mAdapter.getItem(position) + " on row number " + position);
+    }
+
+    // Para añadir nuevos elementos consultar: https://stackoverflow.com/questions/31367599/how-to-update-recyclerview-adapter-data/48959184#48959184
+    public void boton(View v) {
+        myDataSet.add("Hola soy Vicente");
+        mAdapter.notifyDataSetChanged();
     }
 }
---
+++ Ejercicio_01/app/src/main/java/com/example/ejercicio_01/MyAdapter.java
@@ -0,0 +1,90 @@
+package com.example.ejercicio_01;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    private ArrayList<String> mDataSet;
+    private ItemClickListener mClickListener;
+
+    /**
+     * Constructor al que le pasamos como parámetro los datos
+     * @param mDataSet
+     */
+    public MyAdapter(ArrayList<String> mDataSet) {
+        this.mDataSet = mDataSet;
+    }
+
+    /**
+     * El layout manager llama a este método
+     * @param parent
+     * @param viewType
+     * @return
+     */
+    @NonNull
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fila, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    /**
+     * Este método reemplaza en contenido de las vistas
+     * @param holder
+     * @param position
+     */
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
+        holder.textView.setText(mDataSet.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataSet.size();
+    }
+
+    // convenience method for getting data at click position
+    public String getItem(int pos) {
+        return mDataSet.get(pos);
+    }
+
+    /**
+     * Aquí utilizamos toas las referencias a las vistas a mostrar en cada fila
+     */
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView textView;
+
+        public MyViewHolder(View v) {
+            super(v);
+            // Debemos tener acceso a las vistas a través de la vista que nos llega (LinearLayout)
+            textView = v.findViewById(R.id.textView);
+            textView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null)
+                mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+
+    // parent activity will implement this method to respond to click events
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    // allows clicks events to be caught
+    void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+}
